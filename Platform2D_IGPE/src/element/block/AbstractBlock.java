925a5e19988c9ca1cb99a5972beb77aee73d19ea
vincenzo <vincenzo@pc-Vincenzino>
2016-03-10 16:20:00 +0100

    ok

--- Platform2D_IGPE/src/element/block/AbstractBlock.java
+++ Platform2D_IGPE/src/element/block/AbstractBlock.java
@@ -23,9 +23,18 @@ public class AbstractBlock implements Block {
 	public double getY() {
 		return position.getY();
 	}
-// il punto in esame appartiene al blocco?
+
+	// il punto in esame appartiene al blocco?
 	protected boolean contains(double x, double y, int height, int width) {
-		if (position.getX() <= x + (width/2) && position.getY() <= y + (height/2)&& position.getX() + WIDTH >= x && position.getY() + HEIGHT >= y){
+		
+		System.out.println("1 parte "+ position.getX() + " < " +(x + (width / 2)));
+		System.out.println("2 parte "+ position.getY() + "  < " + (y + (height / 2)));
+		System.out.println("3 parte "+ (position.getX() + WIDTH) + " > " + x );
+		System.out.println("4 parte "+ (position.getY() + HEIGHT) + " > " + y);
+		
+		
+		if (position.getX() < x + (width / 2) && position.getY() < y + (height / 2) && position.getX() + WIDTH > x
+				&& position.getY() + HEIGHT > y) {
 			return true;
 		}
 		return false;
@@ -33,11 +42,11 @@ public class AbstractBlock implements Block {
 
 	@Override
 	public boolean collision(double x, double y, int height, int width) {
-			return contains(x, y, height, width);
+		return contains(x, y, height, width);
 	}
-	
+
 	public static void main(String[] args) {
-		StandardBlock s = new StandardBlock(new Position(30, 30));
-		System.out.println("Collision "+s.collision(25, 25, 10, 10));
+		StandardBlock s = new StandardBlock(new Position(0, 0));
+		System.out.println("Collision " + s.collision(9, 9, 10, 10));
 	}
 }

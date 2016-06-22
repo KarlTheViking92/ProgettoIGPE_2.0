df8edc06a04f8cdf98cd0c4c236f2ddc9f59cbbb
Carlo <carlo.rocca@hotmail.it>
2016-06-22 18:58:01 +0200

    prova

---
+++ Platform2D_IGPE/src/core/element/block/ExplosiveBlock.java
@@ -0,0 +1,10 @@
+package core.element.block;
+
+import core.element.Position;
+
+public class ExplosiveBlock extends AbstractBlock{
+
+	public ExplosiveBlock(Position position) {
+		super(position);
+	}
+}
---
+++ Platform2D_IGPE/src/gui/GraphicBlock.java
@@ -0,0 +1,11 @@
+package gui;
+
+import core.element.block.AbstractBlock;
+import javafx.scene.image.ImageView;
+
+public class GraphicBlock {
+
+	private ImageView img;
+	private AbstractBlock block;
+	
+}

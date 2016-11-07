473e6f3cf6791614abfd7093760f9aecfb21dcea
Vincenzo <Vincenzo@pc-vincenzo>
2016-11-07 17:53:20 +0100

    game field

--- Platform2D_IGPE/src/gui/panel/singlePlayer/GameField.java
+++ Platform2D_IGPE/src/gui/panel/singlePlayer/GameField.java
@@ -1,13 +1,25 @@
 package gui.panel.singlePlayer;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import core.element.Item;
+import core.element.block.Block;
 import core.gameManagers.PlayManager;
+import gui.ImageProvider;
+import gui.drawer.CharacterDrawer;
+import gui.element.GraphicBlockFactory;
+import gui.element.GraphicElement;
+import gui.element.GraphicGem;
 import gui.event.KeyboardPressedEvent;
 import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Camera;
+import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.SubScene;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Screen;
 
@@ -15,8 +27,13 @@ public class GameField extends SubScene {
 
 	private static Pane root = new Pane();
 	private Camera camera = new PerspectiveCamera();
+	
 	private PlayManager manager;
 	private SinglePlayerPane gamePanel;
+	private CharacterDrawer drawer;
+	private List<GraphicElement> imgs = new ArrayList<>();
+	private GraphicBlockFactory factory = new GraphicBlockFactory();
+	private Group group = new Group();
 	private Rectangle2D screen = Screen.getPrimary().getBounds();
 	private Point2D widthLimit;
 	private Point2D heightLimit;
@@ -25,17 +42,17 @@ public class GameField extends SubScene {
 		super(root, w, h);
 		manager = PlayManager.getInstance();
 		gamePanel = game;
-//		this.camera.setScaleX(0.5);
-//		this.camera.setScaleY(0.5);
-
+		this.camera.setScaleX(0.5);
+		this.camera.setScaleY(0.5);
+		drawer = new CharacterDrawer(manager.getPlayer());
 		this.setCamera(camera);
-		// this.setOnKeyPressed(new KeyboardPressedEvent(gamePanel));
-		// this.setOnKeyReleased(new KeyboardPressedEvent(gamePanel));
 
 		widthLimit = new Point2D(manager.getLevelWidth() * 0.24, manager.getLevelWidth() * 0.75);
 		heightLimit = new Point2D(manager.getLevelHeight() * 0.15, manager.getLevelHeight() * 0.86);
 		initCamera();
-		root.getChildren().add(gamePanel);
+		root.setStyle("-fx-background: null; -fx-background-color: null; ");
+		root.getChildren().add(group);
+		
 	}
 
 	private void initCamera() {
@@ -61,23 +78,56 @@ public class GameField extends SubScene {
 	}
 
 	public void update() {
+		updateCamera();
+		updateBlocks();
+		drawer.draw();
+	}
+	
+	public void drawWorld() {
+
+		for (int y = manager.getBlocksMatrix().length - 1; y >= 0; y--) {
+			for (int x = 0; x < manager.getBlocksMatrix()[y].length; x++) {
+				Block b = manager.getBlocksMatrix()[y][x];
+				if (b != null) {
+					
+					GraphicElement img = factory.makeBlock(b);
+					imgs.add(img);
+					this.group.getChildren().add((ImageView)img);
+				}
+			}
+		}
+		for (Item gem : manager.getGemList()) {
+			GraphicElement img = new GraphicGem(gem);
+			this.imgs.add(img);
+			this.group.getChildren().add((ImageView)img);
+		}
+//		List<Character> l = manager.getMeleeEnemy();
+//		
+//		MeleeEnemy e  = (MeleeEnemy)l.get(0);
+//		System.out.println(e);
+//		ciccioCattivo = new ImageView(ImageProvider.getInstance().getSpecialBlock("CloudBlock"));
+//		ciccioCattivo.setLayoutX(e.getX());
+//		ciccioCattivo.setLayoutY(e.getY());
+		
+//		this.group.getChildren().add(ciccioCattivo);
+		this.group.getChildren().add(drawer);
+				
+	}
+	
+	private void updateBlocks(){
+		for(GraphicElement block : imgs){
+			block.update();
+		}
+	}
+	
+	private void updateCamera(){
 		double x = manager.getPlayer().getX();
 		double y = manager.getPlayer().getY();
 		if (x > widthLimit.getX() && x < widthLimit.getY()) {
 			this.camera.setTranslateX(manager.getPlayer().getX() - (screen.getWidth() / 4));
-		}else{
-//			System.out.println("se non la muovi la x sta in ");
-//			System.out.println(camera.getTranslateX());
-//			System.out.println(widthLimit.getY());
-//			System.out.println("sei così "+ (widthLimit.getY() - (screen.getWidth() / 4)));
 		}
 		if (y > heightLimit.getX() && y < heightLimit.getY()) {
 			this.camera.setTranslateY(manager.getPlayer().getY() - (screen.getHeight() / 4));
-		}else{
-//			System.out.println("se non la muovi la y sta in ");
-//			System.out.println(camera.getTranslateY());
-//			System.out.println(heightLimit.getY());
-//			System.out.println("sei così "+ (heightLimit.getY() - (screen.getHeight() / 4)));
 		}
 	}
 
